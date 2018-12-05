8553b9859dfb787c076feca72583049a56f80a3a
SarinSwift <sarinyaswift@gmail.com>
2018-12-04 16:33:08 -0800

    added applewatch target

---
+++ WatchMoodTracker/MoodRow.swift
@@ -0,0 +1,13 @@
+//
+//  MoodRow.swift
+//  moodTracker
+//
+//  Created by Sarin Swift on 12/4/18.
+//  Copyright © 2018 sarinswift. All rights reserved.
+//
+
+import WatchKit
+
+class MoodRow: NSObject {
+
+}
