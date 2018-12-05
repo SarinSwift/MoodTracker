ffbf7f0aa6be631508aba22e87ccad57f53d87c4
SarinSwift <sarinyaswift@gmail.com>
2018-12-04 16:34:08 -0800

    added applewatch target

--- WatchMoodTracker/MoodRow.swift
+++ WatchMoodTracker/MoodRow.swift
@@ -9,5 +9,31 @@
 import WatchKit
 
 class MoodRow: NSObject {
-
+    
+    var entries: [MoodEntry] = []
+    
+    @IBOutlet weak var moodImg: WKInterfaceImage!
+    @IBOutlet weak var moodLabel: WKInterfaceLabel!
+    @IBOutlet weak var table: WKInterfaceTable!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let goodEntry = MoodEntry(mood: .good, date: Date())
+        let badEntry = MoodEntry(mood: .bad, date: Date())
+        let neutralEntry = MoodEntry(mood: .neutral, date: Date())
+        let amazingEntry = MoodEntry(mood: .amazing, date: Date())
+        let terribleEnrty = MoodEntry(mood: .terrible, date: Date())
+        
+        entries = [goodEntry, badEntry, neutralEntry, amazingEntry, terribleEnrty]
+        table.setNumberOfRows(entries.count, withRowType: "moodRow")
+    }
+    
+    var moodObj: MoodEntry? {
+        didSet {
+            guard let moodObj = moodObj else { return }
+            moodImg.setImage(UIImage(named: moodObj.mood.stringValue))
+            moodLabel.setText(moodObj.mood.stringValue)
+        }
+    }
+    
 }
